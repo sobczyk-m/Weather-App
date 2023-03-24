674cd8c122d33c7f2cbcddfdea13d8ebecbb8100
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-03-24 14:04:04 +0100

    feat: change background of selected weather period

--- src/components/App/SectionBar/SectionBar.tsx
+++ src/components/App/SectionBar/SectionBar.tsx
@@ -1,14 +1,32 @@
 import styles from "./SectionBar.module.pcss";
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+type Section = "current" | "48h" | "8days" | "air"
 
 function SectionBar() {
+    const [activeSection, setActiveSection] = useState<Section>("current")
+
+    useEffect(() => {
+        changeSection(activeSection)
+    }, [activeSection])
+
+    const changeSection = (section: Section) => {
+        setActiveSection(section)
+        const parent = document.getElementById(styles.bar)!.children
+
+        for (let i = 0; i < parent.length; i++) {
+            parent[i].classList.remove(styles.activeSection)
+        }
+
+        document.getElementById(section)!.classList.add(styles.activeSection)
+    }
 
     return (
-        <div className={styles.bar}>
-            <div>Current</div>
-            <div>48h</div>
-            <div>8 days</div>
-            <div>Air</div>
+        <div id={styles.bar}>
+            <div onClick={() => setActiveSection("current")} id={"current"}>Current</div>
+            <div onClick={() => setActiveSection("48h")} id={"48h"}>48h</div>
+            <div onClick={() => setActiveSection("8days")} id={"8days"}>8 days</div>
+            <div onClick={() => setActiveSection("air")} id={"air"}>Air</div>
         </div>
     )
 }
